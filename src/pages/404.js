f52cda6e7fce8e58db528c7894bce238224cee52
nataliereshetnikova <ab1748@gmail.com>
2020-05-28 15:55:02 -0400

    error page comment

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Layout from 'components/Layout';
 import Container from 'components/Container';
-
+//error page
 const NotFoundPage = () => {
   return (
     <Layout>
